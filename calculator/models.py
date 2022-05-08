dab64cee30dcb9bb1f7bf4831eb7415e05158e16
pawelwisnia12 <47950295+pawelwisnia12@users.noreply.github.com>
2022-05-08 21:44:02 +0200

    Add pizza model and calculator to pizza settings

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Pizza(models.Model):
+    type = models.CharField(max_length=100)
+    published_date = models.DateField("Date published")
