0289951a58dc593bef661f013e2270ec318ff27e
pawelwisnia12 <47950295+pawelwisnia12@users.noreply.github.com>
2022-05-10 20:52:10 +0200

    Add new Pizza fields

--- calculator/models.py
+++ calculator/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Pizza(models.Model):
     type = models.CharField(max_length=100)
     published_date = models.DateField("Date published")
+    size = models.IntegerField(default=0)
+    hydro = models.FloatField(default=0.0)
