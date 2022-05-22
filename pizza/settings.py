7e871b316475c904bab9cb0ab34caeaac860d25a
pawelwisnia12 <47950295+pawelwisnia12@users.noreply.github.com>
2022-05-22 21:40:25 +0200

    CR changes

--- pizza/settings.py
+++ pizza/settings.py
@@ -53,8 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-
 ]
 
 ROOT_URLCONF = 'pizza.urls'
