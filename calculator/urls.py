b04cf4e701669eedf960a2a697825c0400e53334
pawelwisnia12 <47950295+pawelwisnia12@users.noreply.github.com>
2022-05-22 21:38:04 +0200

    Add POST all pizzas

--- calculator/urls.py
+++ calculator/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.home, name='home'),
     path("calculate", views.calculate, name='calculate'),
     path("all_pizzas", views.all_pizzas, name='all_pizzas'),
-    re_path(r'^pizzas$',views.pizza_list),
+    re_path(r'^pizzas$', views.pizza_list),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, JsonResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 
 from calculator.models import Pizza
 from calculator.serializers import PizzaSerializer
@@ -18,8 +20,16 @@ def all_pizzas(request):
     return JsonResponse(pizzas, safe=False)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def pizza_list(request):
-    pizzas = Pizza.objects.all()
-    pizzas_serializer = PizzaSerializer(pizzas, many=True)
-    return JsonResponse(pizzas_serializer.data, safe=False)
+    if request.method == 'GET':
+        pizzas = Pizza.objects.all()
+        pizzas_serializer = PizzaSerializer(pizzas, many=True)
+        return JsonResponse(pizzas_serializer.data, safe=False)
+    if request.method == 'POST':
+        pizza_data = JSONParser().parse(request)
+        pizza_serializer = PizzaSerializer(data=pizza_data)
+        if pizza_serializer.is_valid():
+            pizza_serializer.save()
+            return JsonResponse(pizza_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(pizza_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
