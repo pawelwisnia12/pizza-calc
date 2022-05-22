404024fe320375db2eb15316cbad74f4f0813721
pawelwisnia12 <47950295+pawelwisnia12@users.noreply.github.com>
2022-05-22 21:13:43 +0200

    Add GET all pizzas

--- calculator/views.py
+++ calculator/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
 
 from calculator.models import Pizza
+from calculator.serializers import PizzaSerializer
 
 
 def calculate(request):
@@ -14,3 +16,10 @@ def home(request):
 def all_pizzas(request):
     pizzas = list(Pizza.objects.values())
     return JsonResponse(pizzas, safe=False)
+
+
+@api_view(['GET'])
+def pizza_list(request):
+    pizzas = Pizza.objects.all()
+    pizzas_serializer = PizzaSerializer(pizzas, many=True)
+    return JsonResponse(pizzas_serializer.data, safe=False)
